a778e724297bd1d8a3f9502517df58f43faaf9dc
Castilho, N <nuno_castilho78@hotmail.com>
2023-01-21 23:59:49 +0000

    re-generated project with appDir

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: false,
-  },
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+}
 }
 
 module.exports = nextConfig
