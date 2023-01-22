0aecff75c1e3a994608d3cc322ec97980d794b40
Nuno Castilho <nuno_castilho78@hotmail.com>
2023-01-22 00:03:27 +0000

    deploy to s3 (#2)
    * deploy to s3
    
    * fix yarn install
    
    * allowing branch deploy
    
    * triger ci
    
    * re-generated project with appDir

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+}
 }
 
 module.exports = nextConfig
